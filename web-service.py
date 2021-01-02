6e461fd0b295a573eb4547420f8c19c0730986b5
Anthony Moore <amooretmp@gmail.com>
2021-01-02 15:44:01 +0000

    added API and front end client

--- web-service.py
+++ web-service.py
@@ -1,22 +1,34 @@
 # flask for web app.
 import flask as fl
-# numpy for numerical work.
+from flask import request, jsonify
+# numpy for numerical arrays.
 import numpy as np
+# tensorflow nerural network
+import tensorflow as tf
+# keras API
+from tensorflow import keras 
 
 # Create a new web app.
 app = fl.Flask(__name__)
+
+# function to load wind turbine prediction model
+def load_keras_model():
+	# load in pre-trained model
+	return keras.models.load_model('powermodel.h5')
+
+# load prediction model
+model = load_keras_model()
 
 # Add root route.
 @app.route("/")
 def home():
   return app.send_static_file('index.html')
 
-# Add uniform route.
-@app.route('/api/uniform')
-def uniform():
-  return {"value": np.random.uniform()}
-
-# Add normal route.
-@app.route('/api/normal')
-def normal():
-  return {"value": np.random.normal()}
+# Add api route.
+@app.route('/api', methods=["POST"])
+def getPower():
+	input = request.form['speed'] 
+	num = float(input)
+	prediction = model.predict([num])
+	result = prediction[0][0].tolist()
+	return {"value": result}
